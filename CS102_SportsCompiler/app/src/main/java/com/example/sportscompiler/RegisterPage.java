bb84716e29cb5dce9a52797a5d78a4b0c61a6203
Burak-5 <burak.yilmaz8745@gmail.com>
2024-11-06 22:21:24 +0300

    Tarih ve şifre
    Tarihler seçiliyor. Şifreler noktayla gözüküyor

--- CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/RegisterPage.java
+++ CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/RegisterPage.java
@@ -1,16 +1,21 @@
 package com.example.sportscompiler;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,6 +23,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class RegisterPage extends Fragment {
 
@@ -46,7 +55,6 @@ public class RegisterPage extends Fragment {
         registerMail = view.findViewById(R.id.registerMail);
         registerPassword = view.findViewById(R.id.registerPassword);
         registerRePassword = view.findViewById(R.id.registerRePassword);
-        birthDate = view.findViewById(R.id.birthDate);
         department = view.findViewById(R.id.department);
         registerButton = view.findViewById(R.id.registerButton);
         firebaseAuth = FirebaseAuth.getInstance(); //initializing firebase authentication
@@ -85,6 +93,70 @@ public class RegisterPage extends Fragment {
         return  view;
     }
 
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        Spinner daySpinner = view.findViewById(R.id.day_spinner);
+        Spinner monthSpinner = view.findViewById(R.id.month_spinner);
+        Spinner yearSpinner = view.findViewById(R.id.year_spinner);
+
+        List<String> days = new ArrayList<>();
+
+        for(int i = 0; i <= 31; i++)
+        {
+            days.add(String.valueOf(i));
+        }
+
+        List<String> months = new ArrayList<>();
+        months.add("January");
+        months.add("February");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("August");
+        months.add("September");
+        months.add("October");
+        months.add("November");
+        months.add("December");
+
+        List<String> years = new ArrayList<>();
+
+        for(int i = 1970; i <= 2024; i++)
+        {
+            years.add(String.valueOf(i));
+        }
+
+        ArrayAdapter<String> dayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, days);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(dayAdapter);
+        daySpinner.setDropDownVerticalOffset(500);
+
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, months);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monthSpinner.setAdapter(monthAdapter);
+        monthSpinner.setDropDownVerticalOffset(500);
+
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, years);
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yearSpinner.setAdapter(yearAdapter);
+        yearSpinner.setDropDownVerticalOffset(500);
+
+
+        birthDate = view.findViewById(R.id.birthDate);
+        birthDate.setOnClickListener(v ->
+        {
+            String selecetedDay = daySpinner.getSelectedItem().toString();
+            String selectedMonth = monthSpinner.getSelectedItem().toString();
+            String selectedYear = yearSpinner.getSelectedItem().toString();
+
+            String date = selecetedDay + "/" + selectedMonth + "/" + selectedYear;
+            birthDate.setText(date);
+        });
+
+    }
     private void createAccount(String email, String password)
     {
         firebaseAuth.createUserWithEmailAndPassword(email, password)

2ca01bab07297d8c9cf562306db71f4c6dd4c6db
Çağan Aksoy <caganaksoyyy@gmail.com>
2024-11-16 23:21:16 +0300

    Added navigator view

--- CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/homeActivity.java
+++ CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/homeActivity.java
@@ -1,24 +1,41 @@
 package com.example.sportscompiler;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class homeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Set default fragment
+        loadFragment(new MainFragment());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.matches:
+                        fragment = new MainFragment();
+                        break;
+                    case R.id.home:
+                        fragment = new MatchAttendanceFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                return loadFragment(fragment);
+            }
         });
     }
 }

103d8b056e4df4a0e072545a368dfe736dbb9e71
Çağan Aksoy <caganaksoyyy@gmail.com>
2024-12-05 01:27:45 +0300

    5x5 Match
    5x5 maç yapma + create match constraini 3. defa yaptım bi hata var error veriyor ve bozuyor

--- CS102_SportsCompiler/app/build.gradle.kts
+++ CS102_SportsCompiler/app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity:1.9.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.leanback:leanback:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

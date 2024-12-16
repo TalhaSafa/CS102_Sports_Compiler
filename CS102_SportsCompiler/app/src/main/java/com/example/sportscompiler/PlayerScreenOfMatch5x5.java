2e630c02af75c2d355840a168bc966227e8fc554
Talha Safa Küçük <talhasafakucuk@gmail.com>
2024-12-16 14:38:04 +0300

    Added pushRating

--- CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/PlayerScreenOfMatch5x5.java
+++ CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/PlayerScreenOfMatch5x5.java
@@ -1,14 +1,25 @@
 package com.example.sportscompiler;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.sportscompiler.AdditionalClasses.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class PlayerScreenOfMatch5x5 extends AppCompatActivity {
+
+    private FirebaseFirestore firestore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,45 @@ public class PlayerScreenOfMatch5x5 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void pushRating(double rating, String userID)
+    {
+        firestore = FirebaseFirestore.getInstance();
+        firestore.collection("users").document(userID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User user = documentSnapshot.toObject(User.class);
+                double totalRating = user.getRatingNumber() * user.getAverageRating();
+                totalRating += rating;
+                user.addRatingNumber();
+                user.setAverageRating(totalRating / user.getRatingNumber());
+
+                firestore.collection("users").document(userID).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(PlayerScreenOfMatch5x5.this, "Rated Player", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(PlayerScreenOfMatch5x5.this, "Could not rate player", Toast.LENGTH_SHORT).show();
+                        double totalRating = user.getRatingNumber() * user.getAverageRating();
+                        totalRating -= rating;
+                        user.decreaseRatingNumber();
+                        user.setAverageRating(totalRating / user.getRatingNumber());
+
+                    }
+
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PlayerScreenOfMatch5x5.this, "Could not access player", Toast.LENGTH_SHORT).show();
+
+            }
         });
     }
 }

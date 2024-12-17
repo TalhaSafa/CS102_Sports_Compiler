30d3841be91b6910071379a7e76ba2ad628af5a3
Burak-5 <burak.yilmaz8745@gmail.com>
2024-12-17 21:43:21 +0300

    5x5 e de koyduk

--- CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/AdminReportPageActivity.java
+++ CS102_SportsCompiler/app/src/main/java/com/example/sportscompiler/AdminReportPageActivity.java
@@ -1,6 +1,7 @@
 package com.example.sportscompiler;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AdminReportPageActivity extends AppCompatActivity {
 
+    private Button sendButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_report_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
